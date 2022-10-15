fa1a4bcfc0ad69818d6b6f3c3f0890717dc2d785
rafalgw59 <r.gaw59lik@gmail.com>
2022-10-15 22:26:58 +0200

    WIP routing v2

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller;
 
+use App\Entity\Location;
+use App\Repository\MeasurementRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class WeatherController extends AbstractController
 {
-    #[Route('/weather', name: 'app_weather')]
-    public function index(): Response
+
+    public function cityAction(Location $city, Location $country, MeasurementRepository $measurementRepository): Response
     {
-        return $this->render('weather/index.html.twig', [
-            'controller_name' => 'WeatherController',
+        $measurements = $measurementRepository->findByLocation($city,$country);
+
+        return $this->render('weather/city.html.twig', [
+            'city' => $city,
+            'country' => $country,
+            'measurements' => $measurements,
         ]);
     }
 }
