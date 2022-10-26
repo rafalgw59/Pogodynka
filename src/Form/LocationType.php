280139babfc814c71441e61185e715c8619454e6
rafalgw59 <r.gaw59lik@gmail.com>
2022-10-26 16:20:57 +0200

    blipblop

--- src/Form/LocationType.php
+++ src/Form/LocationType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Location;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +13,17 @@ class LocationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('country', ChoiceType::class, [
+                'choices'  => [
+                    'PL' => "Polska",
+                    'DE' => "Niemcy",
+                    'LOL' => "Lololand",
+                ],
+                ])
             ->add('city')
-            ->add('country')
             ->add('latitude')
             ->add('longitude')
+
         ;
     }
 
